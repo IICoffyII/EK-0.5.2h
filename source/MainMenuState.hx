1b0a6556c8fe6b6dbe7a8964303318d4c6bae5b3
IICoffyII <83324773+IICoffyII@users.noreply.github.com>
2022-07-03 13:50:44 -0700

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = 'Project discontinued'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = 'Coffy Version 0.5.2h'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 
@@ -39,7 +39,6 @@ class MainMenuState extends MusicBeatState
 		#if MODS_ALLOWED 'mods', #end
 		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
-		#if !switch 'donate', #end
 		'options'
 	];
 
@@ -128,11 +127,11 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Project discontinued", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Coffy Version 0.5.2h", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Project discontinued", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Coffy Version 0.5.2h", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
